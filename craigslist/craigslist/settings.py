3bdd6beddc61a6b1d1fd0f5dc182661c3c06a604
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-10-05 02:29:31 +0530

    initial commit

--- craigslist/craigslist/settings.py
+++ craigslist/craigslist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +125,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+
+django_heroku.settings(locals())
